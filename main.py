5f398ef574ae88e4c5283743b3bfe2c0d0f10d4b
bWX1143153 <brdc.cccxxiii@h-partners.com>
2022-12-11 10:54:19 -0300

    continuando classes

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 # classe Usuario
 
 class Usuario ():
-    def __init__(self):
+    def __init__(self, nome, idade):
+        self.nome = nome.capitalize()
+        self.idade = int(idade)
+        self.maioridade = self.testaMarioridade()
 
-
-
-
+    def testaMarioridade(self):
+        if self.idade >= 18:
+            return "Maior"
+        else:
+            return "Menor"
 
 # fluxo padrão de informações
+def menu_principal ():
+    print("\n\n____menu____")
+    print("1 - Cadastrar novo usuario\n"
+          "2 - verificar lista de usuarios\n"
+          "3 - Ultimo cadastrado\n"
+          "4 - Deletar Item "
+          "S - sair\n")
+    return input()
 
+sair = False
+lista= []
 while sair == False:
     menu = menu_principal()
+    objeto = ""
     if menu == "1":
-        lista = cadastro()
+        nome = input("Nome: ")
+        idade = input("Idade: ")
+        lista.append(Usuario(nome,idade))
     elif menu == "2":
-        usuarios_cadastrados(lista)
+         for objeto in lista:
+             print("{} - {} - {}".format(objeto.nome, objeto.idade, objeto.maioridade))
     elif menu == "3":
-        ultimo_cadastro()
+        objeto = lista[-1]
     elif menu =="4":
-        lista = deletar(lista)
+        nome = input("Nome: ")
     elif menu == "S":
         sair = True
+print("\nprograma finalizado\n")
